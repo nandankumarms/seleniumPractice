d6a356ec7a7497cd447a06b7b252f7ee9544a0c9
nandankumarms <nandankumarms31@gmail.com>
2023-05-22 14:22:01 +0530

    Program to launch Firefox Browser

---
+++ src/Selenium_Basics/LaunchFireFoxBrowser.java
@@ -0,0 +1,20 @@
+package Selenium_Basics;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchFireFoxBrowser {
+	public static void main(String[] args) throws InterruptedException {
+		//Set system Property for driver.
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		
+		//Launch Firefox Browser
+		FirefoxDriver driver=new FirefoxDriver();
+		
+		//Wait for 2 seconds
+		Thread.sleep(2000);
+		
+		//Close driver
+		driver.close();
+	}
+
+}
